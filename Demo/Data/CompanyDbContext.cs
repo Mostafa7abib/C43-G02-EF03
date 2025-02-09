3a86ff1c01c5cb56e3508d2be8d2dce324b3e770
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-09 22:16:02 +0200

    Video 04 - Many to Many Relationship

--- Demo/Data/CompanyDbContext.cs
+++ Demo/Data/CompanyDbContext.cs
@@ -16,6 +16,10 @@ namespace Demo.Data
             => optionsBuilder.UseSqlServer("Server=ABOHABIB\\MSSQLSERVER01;Database=DemoEf03;Trusted_Connection=True;Trustservercertificate=True");
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentsCourses> StudentsCourses { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
