c5bc88c96a00d18f8961c92483b8a26ddc6f3496
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-09 22:59:43 +0200

    Video 05 - Inheritence Relationship {TPCC}

---
+++ PartInheritance/Data/RouteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PartInheritance.Data.Models;
+
+namespace PartInheritance.Data
+{
+    internal class RouteDbContext : DbContext
+    {
+        public DbSet<FullTimeEmployee> fullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> partTimeEmployees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ABOHABIB\\MSSQLSERVER01;Database=DemoEf03Inheritance;Trusted_Connection=True;Trustservercertificate=True");
+        }
+    }
+}
---
+++ PartInheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using PartInheritance.Data;
+using PartInheritance.Data.Models;
+
+namespace PartInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using RouteDbContext dbContext = new RouteDbContext();
+            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee()
+            {
+                Name = "Mostafa",
+                Address = "Helwan",
+                Salary = 3_000,
+                Age = 22,
+                StartDate = DateTime.Now,
+            };
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee()
+            { 
+                Name = "Ahmed",
+                Age = 21,
+                Address = "Alex",
+                CountOfHours = 3,
+                HourRate = 1_00
+            };
+            Console.WriteLine(dbContext.Entry(fullTimeEmployee).State);
+            dbContext.fullTimeEmployees.Add(fullTimeEmployee);
+            Console.WriteLine(dbContext.Entry(fullTimeEmployee).State);
+            dbContext.partTimeEmployees.Add(partTimeEmployee);
+            Console.WriteLine(dbContext.Entry(partTimeEmployee).State);
+            dbContext.SaveChanges();
+            var FTE = (from FT in dbContext.fullTimeEmployees
+                       where FT.Id == 1
+                       select FT).FirstOrDefault();
+            Console.WriteLine(FTE?.Name??"NotFound");
+            Console.WriteLine();
+            var PTE = (from PT in dbContext.partTimeEmployees
+                       where PT.Id == 1
+                       select PT).FirstOrDefault();
+            Console.WriteLine(PTE?.Name ?? "NotFound");
+        }
+    }
+}
